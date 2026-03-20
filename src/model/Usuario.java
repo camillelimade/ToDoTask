74262442f6af091488855aed4174c4580d8bf31a
camille <camilledds0@gmail.com>
2026-03-20 14:57:33 -0300

    feat: adiciona relacionamento entre Usuario e Projetos

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,18 +6,25 @@ public class Usuario extends Entidade{
     private String nome;
     private String email;
     private ArrayList<Task> tasks = new ArrayList<>();
+    private ArrayList<Projeto> projetos = new ArrayList<>();
 
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
-
+    public void adicionarProjeto(Projeto projeto) {
+        projetos.add(projeto);
+    }
+    public ArrayList<Projeto> getProjetos() {
+        return projetos;
+    }
     public Usuario() {
     }
 
     public ArrayList<Task> getTasks() {
         return tasks;
     }
+
     public static void main(String[] args) {
 
 
