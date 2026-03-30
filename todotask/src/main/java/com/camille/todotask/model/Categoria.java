9eb945e8c306f507998b9265505be8224f9b9945
camille <camilledds0@gmail.com>
2026-03-30 10:17:06 -0300

    refactor: reescrevendo a entidade Categoria, e aplicando nela a estrutura Spring

--- todotask/src/main/java/com/camille/todotask/model/Categoria.java
+++ todotask/src/main/java/com/camille/todotask/model/Categoria.java
@@ -1,23 +1,26 @@
-package model;
+package com.camille.todotask.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
-
-    public Categoria(String nome) {
-        this.nome = nome;
+    public Categoria() {} // construtor vazio
+    public Long getId() {
+        return id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    @Override
-    public String toString() {
-        return nome;
-    }
 }
--- todotask/src/main/java/com/camille/todotask/model/Entidade.java
+++
@@ -1,7 +0,0 @@
-package model;
-public abstract class Entidade {
-    protected int id;
-    public int getId() {
-        return id;
-    }
-}
